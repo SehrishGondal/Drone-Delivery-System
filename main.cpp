28af25d8757215cd9e18fa0f7e0550d3d110bb46
Sehrish Jabeen <154544564+SehrishGondal@users.noreply.github.com>
2026-04-23 16:55:51 +0500

    Add files via upload
    Updated route optimization logic
    Added package tracking feature

--- main.cpp
+++ main.cpp
@@ -1,1 +1,7 @@
 cout<<"Drone Delivery System"<<endl;
+cout<<"1. Add Drone"<<endl;
+cout<<"2. Add Package"<<endl;
+cout<<"3. Assign Package to Drone"<<endl;
+cout<<"4. Display Drones"<<endl;
+cout<<"5. Display Packages"<<endl;
+cout<<"6. Exit"<<endl;
